ecf131a87a3e0490d69b3e59fde2a16c6e43a71f
VLKNK <dryadengaming@gmail.com>
2018-07-10 18:11:36 +0200

    Fixed broken field errors, still need things to figure out. Added also some css3

--- src/main/java/com/rsvier/workshop2/controller/LoginController.java
+++ src/main/java/com/rsvier/workshop2/controller/LoginController.java
@@ -1,12 +1,12 @@
 package com.rsvier.workshop2.controller;
 
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,6 +19,10 @@ public class LoginController {
 
 	AccountRepository accountRepository;
 
+	@ModelAttribute("account")
+	public Account getAccount() {
+		return new Account();
+	}
 	@Autowired
 	public LoginController(AccountRepository accountRepository) {
 		this.accountRepository = accountRepository;
@@ -31,19 +35,24 @@ public class LoginController {
 	}
 
 	@PostMapping
-	public String doLogin(@Valid Account account, Model model,Errors errors) {
+	public String doLogin(@Valid Account account,Errors error, Model model) {
 
+		if(error.hasErrors()) {
+			return "login";
+		}
+		
 		if (accountRepository.findByEmail(account.getEmail()) != null) {
 			return "adminMainMenu";
 
 		}
-		String message = "Your informations are wrong. Please try to log in agian.";
+		String message = "Het ingevoerde email adres of paswoord klopt niet, probeer het nogmaals.";
 
 		model.addAttribute("message", message);
 
 		model.addAttribute("account", account);
-
+		
 		return "login";
+	
 	}
 
 }
