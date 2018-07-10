255995aeba86fc3e6f57c36d71354b223e8371b6
evywetch <evywetch@gmail.com>
2018-07-10 11:56:54 +0200

    Add LoginController, LoginFrom.

---
+++ src/main/java/com/rsvier/workshop2/controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.rsvier.workshop2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rsvier.workshop2.domain.Account;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	
+	@GetMapping
+	public String login() {
+		
+		return "login";
+	}
+	
+	@PostMapping
+	public String doLogin(Errors errors , Account account ) {
+		
+		if (errors.hasErrors()) {
+			return "login";
+			}
+		
+		
+		System.out.println(account.getEmail() + account.getPassword());
+		
+		return "adminMainMenu";
+	}
+
+}
