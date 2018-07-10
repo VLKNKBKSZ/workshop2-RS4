aa9d22460929b5886e66da9caf5795df917fe107
VLKNK <dryadengaming@gmail.com>
2018-07-10 10:50:06 +0200

    homepage is working and added some css

--- src/main/java/com/rsvier/workshop2/Workshop2Application.java
+++ src/main/java/com/rsvier/workshop2/Workshop2Application.java
@@ -2,11 +2,20 @@ package com.rsvier.workshop2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class Workshop2Application {
+public class Workshop2Application extends WebMvcConfigurerAdapter{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Workshop2Application.class, args);
+		
+	
 	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+		}
 }
