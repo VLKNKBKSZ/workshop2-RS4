3c3754ddd60d8594341a218534cf477dda4fcb07
evywetch <evywetch@gmail.com>
2018-07-11 11:58:38 +0200

    Create Account form and methods in AccountController.

--- src/main/java/com/rsvier/workshop2/controller/AccountController.java
+++ src/main/java/com/rsvier/workshop2/controller/AccountController.java
@@ -1,6 +1,49 @@
 package com.rsvier.workshop2.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rsvier.workshop2.domain.Account;
+import com.rsvier.workshop2.domain.Account.AccountType;
+import com.rsvier.workshop2.repository.AccountRepository;
+
+@Controller
+@RequestMapping("/account")
 public class AccountController {
-	// testing develop
+
+	AccountRepository accountRepository;
+
+	@Autowired
+	public AccountController(AccountRepository accountRepository) {
+		this.accountRepository = accountRepository;
+	}
+
+	@GetMapping
+	public String showAccountForm() {
+
+		return "register";
+
+	}
+
+	@PostMapping
+	public String doCreateAccount(@Valid @ModelAttribute Account account, Errors errors) {
+
+		if (errors.hasErrors()) {
+			return "account";
+		}
+
+		account.setAccountType(AccountType.CUSTOMER);
+
+		Account accountDB = accountRepository.save(account);
+
+		return "redirect:/person";
+	}
 
 }
