f0f9844c33c76aff621f4cd2192e1e870d93db92
VLKNKBKSZ <dryadengaming@gmail.com>
2018-07-12 16:00:09 +0200

    Fixed address validation forgot, @Valid annotation.

--- src/main/java/com/rsvier/workshop2/controller/AddressController.java
+++ src/main/java/com/rsvier/workshop2/controller/AddressController.java
@@ -1,5 +1,7 @@
 package com.rsvier.workshop2.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -48,7 +50,7 @@ public class AddressController {
 	}
 
 	@PostMapping
-	public String doCreateAddress(Account account, Person person, Address address, Errors errors,SessionStatus sessionstatus) {
+	public String doCreateAddress(Account account, Person person, @Valid Address address, Errors errors,SessionStatus sessionstatus) {
 
 		if(errors.hasErrors()) {
 			return "addressForm";
