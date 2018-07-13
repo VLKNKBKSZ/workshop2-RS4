ef0bff2181b8a0cb3f50650fa8fa38b4307ea322
evywetch <evywetch@gmail.com>
2018-07-13 14:00:50 +0200

    Create customerController to handle customer main menu view. Still have to test.

--- src/main/java/com/rsvier/workshop2/controller/CustomerController.java
+++ src/main/java/com/rsvier/workshop2/controller/CustomerController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.rsvier.workshop2.repository.PersonRepository;
 
 @Controller
 @RequestMapping("/customer")
+@SessionAttributes({"account","person"})
 public class CustomerController {
 	
 	PersonRepository personRepository;
