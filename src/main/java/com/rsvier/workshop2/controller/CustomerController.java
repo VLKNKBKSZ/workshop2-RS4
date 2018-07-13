559c76eb0eac130085b0332cc6ee0b8b87ac3c2e
evywetch <evywetch@gmail.com>
2018-07-13 14:37:10 +0200

    Create personDetail.html to show person details after clicking My account link. create method in CustomerController to show person details.

--- src/main/java/com/rsvier/workshop2/controller/CustomerController.java
+++ src/main/java/com/rsvier/workshop2/controller/CustomerController.java
@@ -2,10 +2,14 @@ package com.rsvier.workshop2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.rsvier.workshop2.domain.Address;
+import com.rsvier.workshop2.domain.Person;
+import com.rsvier.workshop2.repository.AddressRepository;
 import com.rsvier.workshop2.repository.PersonRepository;
 
 @Controller
@@ -14,15 +18,27 @@ import com.rsvier.workshop2.repository.PersonRepository;
 public class CustomerController {
 	
 	PersonRepository personRepository;
+	AddressRepository addressRepository;
 	
 	@Autowired
-	public CustomerController(PersonRepository personRepository) {
+	public CustomerController(PersonRepository personRepository,AddressRepository addressRepository) {
 		this.personRepository = personRepository;
+		this.addressRepository = addressRepository;
 	}
 	
 	@GetMapping
 	public String showCustomerMainMenu() {
 		return"customerMainMenu";
+	}
+	
+	
+	@RequestMapping("/personDetails")
+	public String showPersondetails(Person person,Model model) {
+		
+		Address addressDB = addressRepository.findByPerson(person);
+		model.addAttribute("address", addressDB);
+		
+		return "personDetails";
 	}
 
 }
--- src/main/java/com/rsvier/workshop2/repository/AddressRepository.java
+++ src/main/java/com/rsvier/workshop2/repository/AddressRepository.java
@@ -2,9 +2,12 @@ package com.rsvier.workshop2.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
 import com.rsvier.workshop2.domain.Address;
+import com.rsvier.workshop2.domain.Person;
 
 @Repository
 public interface AddressRepository extends CrudRepository<Address,Long>{
 
+	public Address findByPerson(Person person);
 }
