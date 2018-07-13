868cb00c441a5b19b49e84ebab0b15cc365a7147
evywetch <evywetch@gmail.com>
2018-07-13 15:06:01 +0200

    Add delete , edit profile in profileDetails.html and also add delete, edit funtion in CustomerController. Still have to test.

--- src/main/java/com/rsvier/workshop2/controller/CustomerController.java
+++ src/main/java/com/rsvier/workshop2/controller/CustomerController.java
@@ -4,26 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.rsvier.workshop2.domain.Account;
 import com.rsvier.workshop2.domain.Address;
 import com.rsvier.workshop2.domain.Person;
+import com.rsvier.workshop2.repository.AccountRepository;
 import com.rsvier.workshop2.repository.AddressRepository;
 import com.rsvier.workshop2.repository.PersonRepository;
 
 @Controller
 @RequestMapping("/customer")
-@SessionAttributes({"account","person"})
+@SessionAttributes({"account","person","address"})
 public class CustomerController {
 	
+	AccountRepository accountRepository;
 	PersonRepository personRepository;
 	AddressRepository addressRepository;
 	
 	@Autowired
-	public CustomerController(PersonRepository personRepository,AddressRepository addressRepository) {
+	public CustomerController(PersonRepository personRepository,AddressRepository addressRepository
+			,AccountRepository accountRepository) {
 		this.personRepository = personRepository;
 		this.addressRepository = addressRepository;
+		this.accountRepository = accountRepository;
 	}
 	
 	@GetMapping
@@ -39,6 +45,20 @@ public class CustomerController {
 		model.addAttribute("address", addressDB);
 		
 		return "personDetails";
+	}
+	@PostMapping("/deleteProfile")
+	public String deleteProfile(Person person, Address address,Account account) {
+		
+		accountRepository.delete(account);
+		personRepository.delete(person);
+		
+		addressRepository.delete(address);
+		
+		
+		
+		
+
+		return "home";
 	}
 
 }
