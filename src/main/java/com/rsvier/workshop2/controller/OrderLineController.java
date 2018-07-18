b6e373efdd274c48aed7fb2a64431fef7f619ea0
VLKNKBKSZ <dryadengaming@gmail.com>
2018-07-18 12:56:34 +0200

    form parsing is wrong , checking what is wrong

--- src/main/java/com/rsvier/workshop2/controller/OrderLineController.java
+++ src/main/java/com/rsvier/workshop2/controller/OrderLineController.java
@@ -6,27 +6,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.rsvier.workshop2.domain.Address;
+import com.rsvier.workshop2.domain.Order;
+import com.rsvier.workshop2.domain.OrderLine;
 import com.rsvier.workshop2.domain.Product;
 import com.rsvier.workshop2.repository.ProductRepository;
 
 @Controller
 @RequestMapping("/orderLine")
 public class OrderLineController {
-	
+
 	private ProductRepository productRepository;
-	
+
+	@ModelAttribute("orderLine")
+	public OrderLine getOrderLine() {
+		return new OrderLine();
+	}
+
 	@Autowired
 	public OrderLineController(ProductRepository productRepository) {
 		this.productRepository = productRepository;
 	}
-	
+
 	@GetMapping
-	public String createNewOrderLine(Model model) {
-		Iterable<Product> productList = productRepository.findAll();
-		model.addAttribute("productList", productList);
+	public String showNewOrderLine() {
 		return "createNewOrderLine";
+	}
+	
+	@PostMapping
+	public String createNewOrderLine(Order orderline) {
+		return "home";
 	}
 
 }
